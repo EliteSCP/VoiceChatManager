c34f078ab8c7cc728166945494f1b095df50af5a
Michal78900 <michal.dlugopis@gmail.com>
2021-08-08 21:28:39 +0200

    Finished documentation and stuff

--- src/Api/Utilities/AudioFile.cs
+++ src/Api/Utilities/AudioFile.cs
@@ -1,11 +1,15 @@
 namespace VoiceChatManager.Api.Utilities
 {
     using System;
+    using System.IO;
     using Api.Extensions;
     using Commands;
     using Exiled.API.Features;
     using UnityEngine;
 
+    /// <summary>
+    /// Used for handling playing audio on certain event.
+    /// </summary>
     public class AudioFile
     {
         /// <summary>
@@ -15,17 +19,32 @@ namespace VoiceChatManager.Api.Utilities
         {
         }
 
+        /// <summary>
+        /// Gets the name of the preset or path to the file.
+        /// </summary>
         public string Name { get; private set; } = string.Empty;
 
+        /// <summary>
+        /// Gets the volume of the audio.
+        /// </summary>
         public float Volume { get; private set; } = 100;
 
+        /// <summary>
+        /// Gets the ChannelType on which the audio will be played
+        /// </summary>
         public string ChannelType { get; private set; } = "Intercom";
 
+        /// <summary>
+        /// Gets the position of played audio, if <see cref="ChannelType"/> is set to "Proximity".
+        /// </summary>
         public Vector3 ProximityLocation { get; private set; } = Vector3.zero;
 
+        /// <summary>
+        /// Tries to play an audio.
+        /// </summary>
         public void Play()
         {
-            if (string.IsNullOrEmpty(Name))
+            if (string.IsNullOrEmpty(Name) || !VoiceChatManager.Instance.Config.Presets.ContainsKey(Name) || !File.Exists(Name))
                 return;
 
             string channelType = ChannelType.GetChannelName();
--- src/Configs/PlayOnEvent.cs
+++ src/Configs/PlayOnEvent.cs
@@ -9,55 +9,55 @@ namespace VoiceChatManager.Configs
     public sealed class PlayOnEvent
     {
         /// <summary>
-        /// Gets .
+        /// Gets <see cref="AudioFile"/> played on WaitingForPlayers event.
         /// </summary>
         [Description("Called when the server waits for players:")]
         public AudioFile WaitingForPlayers { get; private set; } = new AudioFile();
 
         /// <summary>
-        /// Gets .
+        /// Gets <see cref="AudioFile"/> played on RoundStarted event.
         /// </summary>
         [Description("Called when the round has started:")]
         public AudioFile RoundStarted { get; private set; } = new AudioFile();
 
         /// <summary>
-        /// Gets .
+        /// Gets <see cref="AudioFile"/> played on NtfRespawn event.
         /// </summary>
         [Description("Called when the NTF are spawned:")]
         public AudioFile NtfEntrance { get; private set; } = new AudioFile();
 
         /// <summary>
-        /// Gets .
+        /// Gets <see cref="AudioFile"/> played on ChaosRespawn event.
         /// </summary>
         [Description("Called when the CI are spawned: (will be heard by ALL players not only ClassD and Chaos!")]
         public AudioFile CiEntrance { get; private set; } = new AudioFile();
 
         /// <summary>
-        /// Gets .
+        /// Gets <see cref="AudioFile"/> played on WarheadStart event.
         /// </summary>
         [Description("Called when the Warhead is started: (the default Alpha Warhead cassie WILL still play!)")]
         public AudioFile WarheadStart { get; private set; } = new AudioFile();
 
         /// <summary>
-        /// Gets .
+        /// Gets <see cref="AudioFile"/> played on WarheadCancel event.
         /// </summary>
         [Description("Called when the Warhead is cancaled: (the default Alpha Warhead cassie WILL still play!)")]
         public AudioFile WarheadCancel { get; private set; } = new AudioFile();
 
         /// <summary>
-        /// Gets .
+        /// Gets <see cref="AudioFile"/> played on WarheadDetonated event.
         /// </summary>
         [Description("Called when the Warhead is detonated:")]
         public AudioFile WarheadDetonated { get; private set; } = new AudioFile();
 
         /// <summary>
-        /// Gets .
+        /// Gets <see cref="AudioFile"/> played on DecontaminationStart event.
         /// </summary>
         [Description("Called when the Decontamination proccess has started: (the default decontamination cassie WILL still play!)")]
         public AudioFile DecontaminationStart { get; private set; } = new AudioFile();
 
         /// <summary>
-        /// Gets .
+        /// Gets <see cref="AudioFile"/> played on RoundEnded event.
         /// </summary>
         [Description("Called when the round has ended:")]
         public AudioFile RoundEnded { get; private set; } = new AudioFile();
